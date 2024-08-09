721d271f0eca0bf0f1ff3cbd0a73794b4c4e5973
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-08-09 15:16:04 +0530

    Added cors value

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://vms-ngo.vercel.app/",
     credentials: true,
   })
 );
