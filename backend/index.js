5c0ef10e23df83de37d480beeca0c2fb88f52d7b
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-08-10 11:45:29 +0530

    Revert "notifications"
    This reverts commit 4aa3aee6eb697e98141ca92dec5e8918ffdccb89.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import { app } from "./src/app.js";
 import connectDB from "./src/db/index.js";
 import dotenv from "dotenv";
-
+import { sendSMS } from "./utils/smsUtil.js";
 dotenv.config({ path: "./.env" });
 
 connectDB()
@@ -19,3 +19,24 @@ connectDB()
     console.log("MongoDB connection failed!!! ", err);
     process.exit(1);
   });
+
+// sendMail();
+
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("express error", error);
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening on ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Connection error ", error);
+//     throw error;
+//   }
+// })();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,13 +19,12 @@ import { Error } from "./components/Error/Error";
 import Loader from "./components/Loader/Loader";
 import axios from "axios";
 import Dashboard from "./components/Dashboard/Dashboard";
-import { toast } from "react-toastify";
 function App() {
   const [data, setData] = useState("");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const requestInterceptor = axios.interceptors.request.use(
+    axios.interceptors.request.use(
       (config) => {
         setLoading(true);
         return config;
@@ -36,40 +35,17 @@ function App() {
       }
     );
 
-    const responseInterceptor = axios.interceptors.response.use(
+    axios.interceptors.response.use(
       (response) => {
         setLoading(false);
         return response;
       },
       (error) => {
         setLoading(false);
-        let errorMessage = "An unknown error occurred";
-        if (error.response && error.response.data) {
-          errorMessage = error.response.data;
-          if (
-            errorMessage.includes("User with email or number already exists")
-          ) {
-            errorMessage = "User with email or number already exists";
-          }
-        }
-        toast.error(errorMessage, {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
         return Promise.reject(error);
       }
     );
-
-    return () => {
-      axios.interceptors.request.eject(requestInterceptor);
-      axios.interceptors.response.eject(responseInterceptor);
-    };
-  }, [setLoading]);
+  }, []);
 
   const route = createBrowserRouter(
     createRoutesFromElements(
