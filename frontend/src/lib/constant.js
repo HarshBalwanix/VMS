302948b49e9bf6773364ff1531efbd0b21bf2fd3
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-12-22 20:00:47 +0530

    Mods done

--- frontend/src/lib/constant.js
+++ frontend/src/lib/constant.js
@@ -1,3 +1,4 @@
-const backendUrl = "http://localhost:8000/api/v1";
+// const backendUrl = "http://localhost:8000/api/v1";
+const backendUrl = "https://vms-ngo-backend.vercel.app/api/v1";
 
 export { backendUrl };
