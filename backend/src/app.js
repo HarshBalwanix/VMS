42f6221b45b973d45c25d648d23a413387d2a7b7
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-12-22 21:31:14 +0530

    Final Commit in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin: "https://vms-ngo.vercel.app",
+    // origin: "http://localhost:5173",
+    origin: "https://vms-ngo.vercel.app",
+
     credentials: true,
   })
 );
